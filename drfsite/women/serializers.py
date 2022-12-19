421b35d9c35ad0d7d2cc605b0063d4dace833b0d
Shevtsov Sergey <shevadotka@gmail.com>
2022-12-19 15:37:30 +0200

    serializers base level

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,8 +1,41 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+# class CreateWomenModel:
+#     '''Создания тест модели для работы с json and serializatir'''
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    '''Сериализатор класс Women'''
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_publish = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+# def encode():
+#     '''Кодирование в json-data'''
+#     model = CreateWomenModel('Victoria', 'Content :Shevtsova')
+#     model_finish = WomenSerializer(model)
+#     print(model_finish.data, type(model_finish.data), sep='\n')
+#     json = JSONRenderer().render(model_finish.data)
+#     print(json)
+
+# #
+# # def decode():
+#        """Декодирования с json"""
+# #     stream = io.BytesIO(b'{"title":"Victoria","content":"Content :Shevtsova"}')
+# #     data = JSONParser().parse(stream)
+# #     serializer = WomenSerializer(data=data)
+# #     serializer.is_valid()
+# #     print(serializer.validated_data)
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,9 +1,28 @@
+from django.forms import model_to_dict
 from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.response import Response
+
 from .models import Women, Category
 from .serializers import WomenSerializer
+from rest_framework.views import APIView
 
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        womenlist = Women.objects.all()
+        return Response({'post': WomenSerializer(womenlist,many=True).data})
+
+    def post(self, request):
+        serializator = WomenSerializer(data=request.data)
+        serializator.is_valid(raise_exception=True)
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': WomenSerializer(post_new).data})
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
